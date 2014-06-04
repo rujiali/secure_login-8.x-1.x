bc4268977525a0e555e0610e6f4e6d03f0d33a16
Adam Malone <adam@adammalone.net>
2014-06-04 21:29:10 +1000

    First rewrite of the redirect method using RedirectResponse

--- lib/Drupal/securelogin/SecureloginManager.php
+++ lib/Drupal/securelogin/SecureloginManager.php
@@ -7,10 +7,10 @@
 
 namespace Drupal\securelogin;
 
-use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Config\ConfigFactory;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Defines the securelogin service.
@@ -19,12 +19,19 @@ class SecureloginManager {
 
   protected $request;
   protected $config;
+  protected $response;
 
   public function __construct(Request $request, ConfigFactory $config) {
     $this->request = $request;
     $this->config = $config;
   }
 
+  /**
+   * Alters the action of a form such that it will POST to a secure address.
+   *
+   * @TODO is this method needed anymore?
+   * @param $form
+   */
   public function secureAction(&$form) {
     $securelogin = $this->config->get('securelogin.settings');
     // @TODO does this take in to account forms with external action?
@@ -44,42 +51,31 @@ class SecureloginManager {
   /**
    * Redirects an insecure request to the same path on the secure base URL.
    */
-  function securelogin_secure_redirect() {
-    global $is_https;
+  function redirect() {
+
     // POST requests are not redirected, to prevent unintentional redirects which
     // result in lost POST data. HTTPS requests are also not redirected.
-    if ($is_https || $_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($this->request->isSecure() || $this->request->isMethod('POST')) {
       return;
     }
-    $path = drupal_is_front_page() ? '' : $_GET['q'];
-    $http_response_code = 301;
-    // Do not permit redirecting to an external URL.
-    $options = array('query' => UrlHelper::filterQueryParameters($_GET['q']), 'https' => TRUE, 'external' => FALSE);
+
+    // Get the current uri and convert HTTPS-ify it.
+    $url = str_replace('http://', 'https://', $this->request->getUri());
+    $this->response = new RedirectResponse($url, Response::HTTP_MOVED_PERMANENTLY);
+
     // We don't use drupal_goto() here because we want to be able to use the
     // page cache, but let's pretend that we are.
+    // @TODO investigate if this is still needed
     \Drupal::moduleHandler()->alter('drupal_goto', $path, $options, $http_response_code);
-    // The 'Location' HTTP header must be absolute.
-    $options['absolute'] = TRUE;
-    $url = url($path, $options);
-    $status = "$http_response_code Moved Permanently";
-    $this->response->headers->set('Status', $status);
-    $this->response->headers->set('Location', $url);
-    // Drupal page cache requires a non-empty page body for some reason.
-    print $status;
-    // Mimic drupal_exit() and drupal_page_footer() and then exit.
-    //module_invoke_all('exit', $url);
-    \Drupal::ModuleHandler()->invokeAll('exit', $url);
-    //drupal_session_commit();
+
+    // Mimic the standard functions that run at the end of a request.
     $session_manager = \Drupal::service('session_manager');
     $session_manager->save();
-    if (\Drupal::config('system.performance')->get('cache.page.user_internal') && ($cache = drupal_page_set_cache($this->response, $this->request))) {
-      foreach ($cache->data['headers'] as $name => $value) {
-        $this->response->headers->set($name, $value);
-      }
+
+    if (\Drupal::config('system.performance')->get('cache.page.use_internal')) {
+      drupal_page_set_cache($this->response, $this->request);
     }
-    else {
-      ob_flush();
-    }
-    exit;
+    $this->response->send();
   }
+
 }
